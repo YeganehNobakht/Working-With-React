6a8c6532d679d03b46a1f0aecb09baabc57c996a
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2025-02-04 17:38:32 +0330

    Adding props to card component

--- my-restaurant/src/components/Card.jsx
+++ my-restaurant/src/components/Card.jsx
@@ -1,20 +1,20 @@
 import "./Card.css";
 
-export default function Card() {
+export default function Card({ totalPrice, serviceFee, discount }) {
   return (
     <div className="container">
       <div className="card-info">
         <p>:جمع کل سفارشات</p>
-        <p>0 تومان</p>
+        <p>{totalPrice ? totalPrice : 0} تومان</p>
       </div>
       <div className="card-info">
         <p className="card-info-title">:حق سرویس</p>
-        <p className="card-info-price">0 تومان</p>
+        <p className="card-info-price">{serviceFee ? serviceFee : 0} تومان</p>
       </div>
 
       <div className="card-info">
         <p className="card-info-title">:تخفیف</p>
-        <p className="card-info-price">0 تومان</p>
+        <p className="card-info-price">{discount ? discount : 0} تومان</p>
       </div>
       <div className="discount-wraper">
         <input className="discount" type="text" placeholder="کد تخفیف" />
@@ -23,7 +23,7 @@ export default function Card() {
 
       <div className="card-info totla-payment">
         <p>مبلغ قابل پرداخت</p>
-        <p>0 تومان</p>
+        <p>{totalPrice ? totalPrice + serviceFee - discount : 0} تومان</p>
       </div>
 
       <button>ثبت سفارش</button>
